9f251bb9ad8276f8b7d874eb22fe2df57241a3de
Chawin Hansasuta <6570052821@student.chula.ac.th>
2023-04-19 17:18:20 +0700

    'heroku'

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db');
-const cors = require('cors');
 const app = express();
-app.use(cors())
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet=require('helmet');
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -28,11 +32,29 @@ app.use('/api/v1/appointments', appointments)
 //Cookie parser
 app.use(cookieParser());
 
+app.use(cors())
+
 //Sanitize data
 app.use(mongoSanitize());
 
 //Set security headers
 app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate Limiting
+const limiter=rateLimit({
+    windowsMs:10*60*1000,//10 mins
+    max: 100
+});
+app.use(limiter);
+
+//Prevent http param pollutions
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
 
 
 const PORT = process.env.PORT || 5000;
@@ -45,4 +67,24 @@ process.on('unhandledRejection', (err, promise) => {
     //Close server & exit process
     server.close(() => process.exit(1));
 })
+
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                title: "http://localhost:5000/api/v1"
+            }
+        ],
+    },
+    apis:['./routes/*.js'],
+};
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
